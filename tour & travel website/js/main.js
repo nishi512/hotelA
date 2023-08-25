e5ac787c78ff65f2b98c079af208a54341f26613
nishi512 <131661886+nishi512@users.noreply.github.com>
2023-08-25 16:47:09 +0545

    Add files via upload

---
+++ tour & travel website/js/main.js
@@ -0,0 +1,18 @@
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:15,
+    nav:false,
+    autoplay:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
